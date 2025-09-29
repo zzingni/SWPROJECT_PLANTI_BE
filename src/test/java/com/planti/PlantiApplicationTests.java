28de9dbb03d5cfa3e44fd7894d5b7b0bd0144530
zzingni <wldms001313@gmail.com>
2025-09-29 18:43:32 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/com/planti/PlantiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
