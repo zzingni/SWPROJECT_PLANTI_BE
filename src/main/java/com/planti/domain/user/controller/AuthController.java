32ca75d0d47dd0ff592d1b65343ac12f4b7420ec
zzingni <wldms001313@gamil.com>
2025-10-14 01:37:13 +0900

    feat: AuthController 생성

---
+++ src/main/java/com/planti/domain/user/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.planti.domain.user.controller;
+
+import com.planti.domain.user.dto.request.SignupRequestDto;
+import com.planti.domain.user.service.SignupService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final SignupService signupService;
+
+    @Operation(summary = "회원가입")
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody @Valid SignupRequestDto signupRequestDto) {
+        signupService.signup(signupRequestDto);
+        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+    }
+}
---
+++ src/main/java/com/planti/domain/user/service/SignupService.java
@@ -0,0 +1,43 @@
+package com.planti.domain.user.service;
+
+import com.planti.domain.user.dto.request.SignupRequestDto;
+import com.planti.domain.user.entity.User;
+import com.planti.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class SignupService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public User signup(SignupRequestDto dto) {
+
+        // 중복 체크
+        if (userRepository.existsByLoginId(dto.getLoginId())) {
+            throw new IllegalArgumentException("이미 사용 중인 아이디 입니다.");
+        }
+        if (userRepository.existsByNickname(dto.getNickname())) {
+            throw new IllegalArgumentException("이미 사용 중인 닉네임입니다.");
+        }
+
+        // 비밀번호 인코딩
+        String encodedPassword = passwordEncoder.encode(dto.getPassword());
+
+        // DTO -> Entity
+        User user = dto.toEntity();
+        user.setPassword(encodedPassword);
+
+        // 유저 저장 (PK 확보)
+        User saved = userRepository.save(user);
+
+        // 최종 엔티티 반환
+        return saved;
+    }
+}
