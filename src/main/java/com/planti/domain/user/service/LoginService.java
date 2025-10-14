c88c07fc70e59a7f89246af81dbae97ee7943dd0
zzingni <wldms001313@gamil.com>
2025-10-15 01:18:21 +0900

    feat: login service 로직 구현 및 request dto 생성

--- src/main/java/com/planti/domain/user/service/LoginService.java
+++ src/main/java/com/planti/domain/user/service/LoginService.java
@@ -1,18 +1,45 @@
 package com.planti.domain.user.service;
 
+import com.planti.domain.user.dto.request.LoginUserRequestDto;
+import com.planti.domain.user.dto.response.LoginUserResponseDto;
+import com.planti.domain.user.entity.User;
 import com.planti.domain.user.repository.LoginUserRepository;
 import com.planti.global.security.TokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.awt.image.BandCombineOp;
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
     private final LoginUserRepository loginUserRepository;
     private final TokenProvider tokenProvider;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
+
+    // 로그인
+    public LoginUserResponseDto login(LoginUserRequestDto loginUserRequestDto) {
+        // 입력받은 로그인 아이디로 유저정보 조회
+        User loginUser = loginUserRepository.findUserByLoginId(loginUserRequestDto.getLoginId())
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 아이디입니다."));
+
+        // 입력받은 비밀번호(암호화)값과 조회된 유저의 비밀번호(암호화)값 비교
+        // matches() 메서드는 입력된 비밀번호와 저장된 비밀번호의 해시
+        if(!passwordEncoder.matches(loginUserRequestDto.getPassword(), loginUser.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // 비밀번호가 일치하면 JWT 토큰 생성
+        // UsernamePasswordAuthenticationToken은 인증 정보를 담는 객체로, 로그인 아이디와 권한 정보를 포함
+        // Collections.emptyList()는 권한이 없음을 나타내며, 필요시 권한 정보를 추가할 수 있음
+        // Authentication 객체를 사용하여 유저 정보를 관리하려고 하기 때문에 해당 객체를 생성해야 함
+        Authentication authentication = new UsernamePasswordAuthenticationToken(loginUser.getLoginId(), null, Collections.emptyList());
+        String token = tokenProvider.createToken(authentication);
+        System.out.println("Generated Token: " + token); // 디버그용 출력
+        return new LoginUserResponseDto(token);
+    }
 
 }
