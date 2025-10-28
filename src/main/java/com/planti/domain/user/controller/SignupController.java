e38c8ab07707c7a5c081df877a195ff3467f6322
zzingni <wldms001313@gmail.com>
2025-10-29 03:09:07 +0900

    fix: 인증 관련 코드 수정

--- src/main/java/com/planti/domain/user/controller/SignupController.java
+++ src/main/java/com/planti/domain/user/controller/SignupController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-public class AuthController {
+public class SignupController {
 
     private final SignupService signupService;
 
