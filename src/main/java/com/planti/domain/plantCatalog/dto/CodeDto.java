e499670dd2f1b6b38d5a79861739b3a843686fb6
zzingni <wldms001313@gmail.com>
2025-11-19 01:20:38 +0900

    feat : 농사로 API (코드-코드명) 전용 dto

---
+++ src/main/java/com/planti/domain/plantCatalog/dto/CodeDto.java
@@ -0,0 +1,13 @@
+package com.planti.domain.plantCatalog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CodeDto {
+    private String code;
+    private String codeNm;
+}
