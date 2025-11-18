6b20459bab4d33947c7d687352344c61cdae206f
zzingni <wldms001313@gmail.com>
2025-11-19 01:18:44 +0900

    feat : RestConfig 설정(api xml rest용)

---
+++ src/main/java/com/planti/global/config/RestConfig.java
@@ -0,0 +1,19 @@
+package com.planti.global.config;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class RestConfig {
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .connectTimeout(Duration.ofSeconds(5))
+                .readTimeout(Duration.ofSeconds(10))
+                .build();
+    }
+}
