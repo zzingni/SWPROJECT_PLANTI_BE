aa099eaec6b79fad7acf195db9a37f2315b21106
zzingni <wldms001313@gmail.com>
2025-11-24 12:54:12 +0900

    feat : dto NotBlank 체크

--- src/main/java/com/planti/domain/plantCatalog/dto/PlantNameDto.java
+++ src/main/java/com/planti/domain/plantCatalog/dto/PlantNameDto.java
@@ -1,5 +1,6 @@
 package com.planti.domain.plantCatalog.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PlantNameDto {
+    @NotBlank(message = "식물 이름은 필수입니다.")
     private String plantName;
 }
