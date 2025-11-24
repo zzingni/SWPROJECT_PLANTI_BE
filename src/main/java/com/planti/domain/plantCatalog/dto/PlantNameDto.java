686336c5ca9d2c82ea6fc65a1d4b1f92b8d5a119
zzingni <wldms001313@gmail.com>
2025-11-24 12:53:26 +0900

    feat : 식물 requestDto 생성

---
+++ src/main/java/com/planti/domain/plantCatalog/dto/PlantNameDto.java
@@ -0,0 +1,12 @@
+package com.planti.domain.plantCatalog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PlantNameDto {
+    private String plantName;
+}
