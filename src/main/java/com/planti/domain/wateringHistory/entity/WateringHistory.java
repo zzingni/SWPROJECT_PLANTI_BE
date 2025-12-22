7d566d92f3f0752a0060fc7de3b3b37b358730fc
zzingni <wldms001313@gmail.com>
2025-12-22 14:50:49 +0900

    feat : 물주기 이력 관리 테이블 생성

---
+++ src/main/java/com/planti/domain/wateringHistory/entity/WateringHistory.java
@@ -0,0 +1,30 @@
+package com.planti.domain.wateringHistory.entity;
+
+import com.planti.domain.userplant.entity.UserPlant;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name="watering_history")
+@NoArgsConstructor
+@AllArgsConstructor
+public class WateringHistory {
+    @Id
+    @Column(name="watering_history_id")
+    private long wateringHistoryId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name="companion_plant_id")
+    private UserPlant companionPlant;
+
+    @Column(name="watering_date")
+    private LocalDateTime  wateringDate;
+
+    @Column(name="watering_status")
+    private String wateringStatus;
+}
